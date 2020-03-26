02405dee5231a5e26df1823b5fb15cd9871812e8
Dmitry Visimskikh <drimetolle@gmail.com>
2020-03-26 18:12:56 +0500

    refactoring controllers

--- src/main/java/com/kmzko/service/controllers/DeployController.java
+++ src/main/java/com/kmzko/service/controllers/DeployController.java
@@ -1,10 +1,6 @@
 package com.kmzko.service.controllers;
 
 import com.kmzko.service.domains.Questionnaire;
-import com.kmzko.service.entity.PersonalConveyor;
-import com.kmzko.service.entity.PersonalQuestionnaire;
-import com.kmzko.service.services.deployers.PersonalConveyorDeployer;
-import com.kmzko.service.services.deployers.PersonalQuestionnaireDeployer;
 import com.kmzko.service.services.deployers.QuestionnaireDeployer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,15 +14,9 @@ import java.net.URI;
 @CrossOrigin(origins = "*")
 public class DeployController {
     private final QuestionnaireDeployer questionnaireDeployer;
-    private final PersonalConveyorDeployer personalConveyorDeployer;
-    private final PersonalQuestionnaireDeployer personalQuestionnaireDeployer;
 
-    public DeployController(QuestionnaireDeployer questionnaireDeployer,
-                            PersonalConveyorDeployer personalConveyorDeployer,
-                            PersonalQuestionnaireDeployer personalQuestionnaireDeployer) {
+    public DeployController(QuestionnaireDeployer questionnaireDeployer) {
         this.questionnaireDeployer = questionnaireDeployer;
-        this.personalConveyorDeployer = personalConveyorDeployer;
-        this.personalQuestionnaireDeployer = personalQuestionnaireDeployer;
     }
 
     @PostMapping(value = "/questionnaire")
@@ -41,54 +31,6 @@ public class DeployController {
     @DeleteMapping(value = "/questionnaire/{id}")
     public ResponseEntity<Void> deleteQuestionnaire(@PathVariable long id) {
         if (!questionnaireDeployer.deleteById(id))
-            return ResponseEntity.notFound().build();
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping(value = "/user/conveyor", produces = "application/json")
-    public ResponseEntity<PersonalConveyor> getUserConveyor(@Valid @RequestBody PersonalConveyor body) {
-        return null;
-    }
-
-    @PostMapping(value = "/user/conveyor", produces = "application/json")
-    public ResponseEntity<PersonalConveyor> saveUserConveyor(HttpServletRequest request,
-                                                         @Valid @RequestBody PersonalConveyor body) {
-        return null;
-    }
-
-    @PutMapping(value = "/user/conveyor/{id}", produces = "application/json")
-    public ResponseEntity<PersonalConveyor> changeUserConveyor(@Valid @RequestBody PersonalConveyor body,
-                                                               @PathVariable long id) {
-        return null;
-    }
-
-    @DeleteMapping(value = "/user/conveyor/{id}")
-    public ResponseEntity<Void> deleteUserConveyor(@PathVariable long id) {
-        if (!personalConveyorDeployer.deleteById(id))
-            return ResponseEntity.notFound().build();
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping(value = "/user/questionnaire", produces = "application/json")
-    public ResponseEntity<PersonalQuestionnaire> getUserQuestionnaire(@Valid @RequestBody PersonalQuestionnaire body) {
-        return null;
-    }
-
-    @PostMapping(value = "/user/questionnaire", produces = "application/json")
-    public ResponseEntity<PersonalQuestionnaire> saveUserQuestionnaire(HttpServletRequest request,
-                                                                  @Valid @RequestBody PersonalQuestionnaire body) {
-        return null;
-    }
-
-    @PutMapping(value = "/user/questionnaire/{id}", produces = "application/json")
-    public ResponseEntity<PersonalQuestionnaire> changeUserQuestionnaire(@Valid @RequestBody PersonalQuestionnaire body,
-                                                               @PathVariable long id) {
-        return null;
-    }
-
-    @DeleteMapping(value = "/user/questionnaire/{id}")
-    public ResponseEntity<Void> deleteUserQuestionnaire(@PathVariable long id) {
-        if (!personalQuestionnaireDeployer.deleteById(id))
             return ResponseEntity.notFound().build();
         return ResponseEntity.ok().build();
     }
