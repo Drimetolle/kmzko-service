100a6569ac833ac5b56aa8533b17231458b7049b
Dmitry Visimskikh <drimetolle@gmail.com>
2020-05-15 20:49:32 +0500

    some fix

--- src/main/java/com/kmzko/configurator/repositories/OrderRepo.java
+++ src/main/java/com/kmzko/configurator/repositories/OrderRepo.java
@@ -9,5 +9,4 @@ import java.util.Optional;
 
 @Repository
 public interface OrderRepo extends JpaRepository<Order, Long> {
-    Optional<Order> findOrdersByConveyorProjectId(long id, long project_id);
 }
